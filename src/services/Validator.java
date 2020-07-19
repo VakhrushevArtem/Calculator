bb83e8dfea49098b0fe0d0f60c442034ccedc1e0
HellRaiseR <deathbringer19@mail.ru>
2020-07-19 17:29:59 +0400

    Complete class Validator and ValidationException

--- src/services/Validator.java
+++ src/services/Validator.java
@@ -1,4 +1,22 @@
 package services;
 
+import exception.ValidationException;
+
+import java.util.Arrays;
+
 public class Validator {
+    public static boolean isCorrectly(String mathematicalExpression) throws Exception {
+        if (mathematicalExpression.matches("[[:alpha:]]") || mathematicalExpression.contains("=")) {
+            throw new ValidationException("Выражение содержит недопустимые символы.");
+        }
+        if (Arrays.stream(mathematicalExpression.split(" ")).filter("(" ::equals).count() !=
+                Arrays.stream(mathematicalExpression.split(" ")).filter(")" ::equals).count()) {
+            throw new ValidationException("Неверное количество скобок.");
+        }
+        mathematicalExpression = mathematicalExpression.replace(" ", "");
+        if(mathematicalExpression.matches("[+\\-*^\\\\]{2,}")) {
+            throw new ValidationException("Выражение записано неверно");
+        }
+        return true;
+    }
 }
